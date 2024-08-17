783d143888385de666a2e0b39c506dc71467eef2
Ibrahim Merad <ibrahimmerad@Ordinateur-portable-de-Ibrahim.local>
2024-08-17 22:35:25 +0200

    changed bce loss to focal loss for Yolov8

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -172,6 +172,7 @@ class v8DetectionLoss:
 
         m = model.model[-1]  # Detect() module
         self.bce = nn.BCEWithLogitsLoss(reduction="none")
+        self.focal_loss = FocalLoss()
         self.hyp = h
         self.stride = m.stride  # model strides
         self.nc = m.nc  # number of classes
@@ -252,7 +253,8 @@ class v8DetectionLoss:
 
         # Cls loss
         # loss[1] = self.varifocal_loss(pred_scores, target_scores, target_labels) / target_scores_sum  # VFL way
-        loss[1] = self.bce(pred_scores, target_scores.to(dtype)).sum() / target_scores_sum  # BCE
+        # loss[1] = self.bce(pred_scores, target_scores.to(dtype)).sum() / target_scores_sum  # BCE
+        loss[1] = self.focal_loss(pred_scores, target_scores.to(dtype)).sum() / target_scores_sum  # Focal loss
 
         # Bbox loss
         if fg_mask.sum():
